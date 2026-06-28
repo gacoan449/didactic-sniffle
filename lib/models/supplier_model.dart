62a5c57ded43296cc1e1a1048769ffc8bf1b22de
Putrasahid <putrasahid@email.com>
2026-06-28 15:51:08 +0700

    Tambah model data pemasok

---
+++ lib/models/supplier_model.dart
@@ -0,0 +1,16 @@
+class Supplier {
+  final String id;
+  final String nama;
+  final String jenis;
+
+  final String alamat;
+  final String noHp;
+
+  Supplier({
+    required this.id,
+    required this.nama,
+    required this.jenis,
+    required this.alamat,
+    required this.noHp,
+  });
+}
