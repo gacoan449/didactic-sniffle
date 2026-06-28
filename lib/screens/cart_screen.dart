165a98871d8e5d4dfe59c2ee01a1bfd89a5abc43
Putrasahid <putrasahid@email.com>
2026-06-28 21:56:25 +0700

    Lengkapi tampilan dan fungsi halaman Keranjang Belanja

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,20 +1,75 @@
 import 'package:flutter/material.dart';
 
-class CartScreen extends StatelessWidget {
+import '../services/cart_service.dart';
+
+class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
 
   @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+
       appBar: AppBar(
         title: const Text("Keranjang"),
       ),
-      body: const Center(
-        child: Text(
-          "Keranjang Belanja",
-          style: TextStyle(fontSize: 22),
-        ),
+
+      body: ListView.builder(
+
+        itemCount: CartService.items.length,
+
+        itemBuilder: (context,index){
+
+          var item = CartService.items[index];
+
+          return ListTile(
+
+            leading: const Icon(Icons.shopping_basket),
+
+            title: Text(item.product.nama),
+
+            subtitle: Text(
+              "Rp ${item.product.harga}"
+            ),
+
+            trailing: Text(
+              "x${item.qty}"
+            ),
+
+          );
+
+        },
+
       ),
+
+      bottomNavigationBar: Container(
+
+        padding: const EdgeInsets.all(15),
+
+        child: Text(
+
+          "Total : Rp ${CartService.total}",
+
+          style: const TextStyle(
+
+            fontSize:20,
+
+            fontWeight: FontWeight.bold,
+
+          ),
+
+        ),
+
+      ),
+
     );
+
   }
+
 }
