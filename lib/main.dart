a60027033280050dcd55bb0618e5e3e511765120
Putrasahid <putrasahid@email.com>
2026-06-28 20:45:50 +0700

    Perbarui rute halaman

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,20 @@
 import 'package:flutter/material.dart';
-
-import 'screens/splash_screen.dart';
-import 'screens/login_screen.dart';
-import 'screens/home_screen.dart';
+import 'routes.dart';
 
 void main() {
-  runApp(const PetaniDesaBerkahApp());
+  runApp(const PetaniDesaApp());
 }
 
-class PetaniDesaBerkahApp extends StatelessWidget {
-  const PetaniDesaBerkahApp({super.key});
+class PetaniDesaApp extends StatelessWidget {
+  const PetaniDesaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Petani Desa Berkah",
       debugShowCheckedModeBanner: false,
-
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.green,
-        ),
-        useMaterial3: true,
-      ),
-
-      initialRoute: "/",
-
-      routes: {
-        "/": (context) => const SplashScreen(),
-        "/login": (context) => const LoginScreen(),
-        "/home": (context) => const HomeScreen(),
-      },
+      title: "Petani Desa Berkah",
+      initialRoute: AppRoutes.home,
+      onGenerateRoute: AppRoutes.generateRoute,
     );
   }
 }
