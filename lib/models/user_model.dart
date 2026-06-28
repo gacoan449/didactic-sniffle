deb58e20a6451e5a3f83f19b24c4b6cc2ce76768
Putrasahid <putrasahid@email.com>
2026-06-28 15:47:04 +0700

    Model pengguna lengkap

---
+++ lib/models/user_model.dart
@@ -0,0 +1,21 @@
+class UserModel {
+  final String id;
+  final String nama;
+  final String nik;
+  final String noHp;
+  final String alamat;
+  final String tipeMember;
+  final int saldo;
+  final int poin;
+
+  UserModel({
+    required this.id,
+    required this.nama,
+    required this.nik,
+    required this.noHp,
+    required this.alamat,
+    required this.tipeMember,
+    required this.saldo,
+    required this.poin,
+  });
+}
