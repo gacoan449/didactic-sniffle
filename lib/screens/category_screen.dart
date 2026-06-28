8f3dcc250b2c86e4c3a50d3ea334e2b91fc9dd0b
Putrasahid <putrasahid@email.com>
2026-06-28 20:48:02 +0700

    Tambah halaman Kategori

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Kategori"),
+      ),
+      body: const Center(
+        child: Text(
+          "Halaman Kategori",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
