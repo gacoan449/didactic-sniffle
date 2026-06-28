4010545d6067345f253d80b094549b6995cb0de2
Putrasahid <putrasahid@email.com>
2026-06-28 22:38:37 +0700

    Tambah model data Obrolan/Pesan

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,24 @@
+class ChatModel {
+
+  final String pengirim;
+  final String penerima;
+  final String pesan;
+  final String waktu;
+  final bool dibaca;
+  final String? foto;
+  final String? orderId;
+
+  ChatModel({
+
+    required this.pengirim,
+    required this.penerima,
+    required this.pesan,
+    required this.waktu,
+    required this.dibaca,
+
+    this.foto,
+    this.orderId,
+
+  });
+
+}
