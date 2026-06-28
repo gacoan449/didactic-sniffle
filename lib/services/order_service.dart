793ece040bf54aebb1f2afe1d7d4863d58009a85
Putrasahid <putrasahid@email.com>
2026-06-28 22:29:28 +0700

    Tambah layanan pengelolaan data Pesanan

---
+++ lib/services/order_service.dart
@@ -0,0 +1,13 @@
+import '../models/order_model.dart';
+
+class OrderService {
+
+  static List<OrderModel> orders=[];
+
+  static void tambah(OrderModel order){
+
+    orders.add(order);
+
+  }
+
+}
