f908739ee562d4b1b86b595b2e8e3b90ec049339
Putrasahid <putrasahid@email.com>
2026-07-01 10:15:44 +0700

    Hubungkan Firebase Selesai Manual

---
+++ lib/firebase_options.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyD9eK8zX7yQzL4bF9rT8wQ5X7z9aP3s2d1f0g9h8j7k6l5",
+      appId: "1:357226488359:android:b9c8d7e6f5a4b3c2d1e0f9a8b7c6d5e4",
+      messagingSenderId: "357226488359",
+      projectId: "petani-desa-berkah",
+      storageBucket: "petani-desa-berkah.appspot.com",
+    );
+  }
+}
